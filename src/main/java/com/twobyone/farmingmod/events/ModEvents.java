a631d91aec14b3f729a7cc1ca0f4c37f95d747e8
pvwnthem <pvwn.them@gmail.com>
2023-05-29 01:23:02 -0400

    add the chance to get an enchanted crop (testing)

--- src/main/java/com/twobyone/farmingmod/events/ModEvents.java
+++ src/main/java/com/twobyone/farmingmod/events/ModEvents.java
@@ -1,25 +1,72 @@
 package com.twobyone.farmingmod.events;
+import java.util.Random;
 
 import com.twobyone.farmingmod.FarmingMod;
+import com.twobyone.farmingmod.items.Items;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
 
 @Mod.EventBusSubscriber(modid = FarmingMod.MODID)
 public class ModEvents {
 
+    public static boolean checkRandomChance(int n) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(n);
+
+        return randomNumber == 0;
+    }
 
 
 
+
+
+
+    public static void onCropBreak(String tier, BlockEvent.BreakEvent event) {
+        ItemStack ENCHANTED_CARROT = new ItemStack(Items.ENCHANTED_CARROT.get());
+        int value = tier.equals("basic") ? 1 : 5;
+        ENCHANTED_CARROT.setCount(value);
+
+        if (checkRandomChance(50)) {
+            event.getPlayer().getInventory().add(ENCHANTED_CARROT);
+            event.getPlayer().sendSystemMessage(Component.literal("Congratulations! You have received an enchanted carrot."));
+
+        }
+
+    }
     @SubscribeEvent
     public static void onBlockBreak(BlockEvent.BreakEvent event) {
         BlockState state = event.getState();
         if (state.getBlock() instanceof CropBlock) {
+            String helditemname = event.getPlayer().getItemInHand(InteractionHand.MAIN_HAND).getDisplayName().getString();
+            String blockname = state.getBlock().getName().getString();
+            System.out.println(helditemname + blockname);
+            if (helditemname.contains("Hoe")) {
+                String tier =  helditemname.contains("Basic") ? "basic" : "advanced";
+                if(helditemname.contains("Carrot") && blockname.equals("Carrots")) {
+                    onCropBreak(tier, event);
+                }
+                if(helditemname.contains("Wheat") && blockname.equals("Wheat Crops")) {
+                    onCropBreak(tier, event);
+                }
+                if (helditemname.contains("Potato") && blockname.equals("Potatoes")) {
+                    onCropBreak(tier, event);
+                }
+            }
 
-            event.getPlayer().getInventory();
+
+                    //.getDisplayName().getString().contains("Basic")
+
         }
     }
 
 }
+
--- src/main/java/com/twobyone/farmingmod/items/ItemPropertyClasses.java
+++ src/main/java/com/twobyone/farmingmod/items/ItemPropertyClasses.java
@@ -1,7 +1,16 @@
 package com.twobyone.farmingmod.items;
 
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.awt.*;
+import java.util.List;
 
 public class ItemPropertyClasses {
     public static class EnchantmentGlintItem extends Item {
